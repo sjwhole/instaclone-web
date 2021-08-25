b0fb038b6b8fd45620e347f1f904d32d650f7cce
sjwhole <bluemoon2718@naver.com>
2021-08-25 19:57:27 +0900

    feat: Add login page

---
+++ src/screens/Login.js
@@ -0,0 +1,51 @@
+import styled from "styled-components";
+import {
+  faFacebookSquare,
+  faInstagram,
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import routes from "../routes";
+import AuthLayout from "../components/auth/AuthLayout";
+import Button from "../components/auth/Button";
+import Separator from "../components/auth/Separator";
+import Input from "../components/auth/Input";
+import FormBox from "../components/auth/FormBox";
+import BottomBox from "../components/auth/BottomBox";
+
+const FacebookLogin = styled.div`
+  color: #385285;
+  span {
+    margin-left: 10px;
+    font-weight: 600;
+  }
+`;
+
+const Login = () => {
+  return (
+    <AuthLayout>
+      <FormBox>
+        <div id="logo">
+          <FontAwesomeIcon icon={faInstagram} size="3x" />
+          <span>SJ스타그램</span>
+        </div>
+        <form>
+          <Input type="text" placeholder="Username" />
+          <Input type="password" placeholder="Password" />
+          <Button type="submit" value="Log in" />
+        </form>
+        <Separator />
+        <FacebookLogin>
+          <FontAwesomeIcon icon={faFacebookSquare} />
+          <span>Log in with Facebook</span>
+        </FacebookLogin>
+      </FormBox>
+      <BottomBox
+        cta="Don't have an account?"
+        linkText="Sign up"
+        link={routes.signUp}
+      />
+    </AuthLayout>
+  );
+};
+
+export default Login;
