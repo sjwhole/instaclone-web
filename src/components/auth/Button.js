f7cf3e0fc4ee12b53a261ec852b0c511124504b9
sjwhole <bluemoon2718@naver.com>
2021-08-25 19:58:59 +0900

    feat: Add button component

---
+++ src/components/auth/Button.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const Button = styled.input`
+  border: none;
+  margin-top: 12px;
+  background-color: ${(props) => props.theme.blue};
+  color: white;
+  text-align: center;
+  padding: 8px 0px;
+  font-weight: 600;
+  width: 100%;
+`;
+
+export default Button;
