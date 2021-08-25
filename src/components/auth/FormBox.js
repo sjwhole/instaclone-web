4484aee4ab51006718551c7a67352734088ed370
sjwhole <bluemoon2718@naver.com>
2021-08-25 19:59:15 +0900

    feat: Add form box component

---
+++ src/components/auth/FormBox.js
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+import { BaseBox } from "../shared";
+
+const Container = styled(BaseBox)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  padding: 35px 40px 25px 40px;
+  margin-bottom: 10px;
+  #logo {
+    display: flex;
+    justify-items: center;
+    flex-direction: column;
+    align-items: center;
+    span {
+      margin-top: 3px;
+    }
+  }
+  form {
+    margin-top: 15px;
+    width: 100%;
+    display: flex;
+    justify-items: center;
+    flex-direction: column;
+    align-items: center;
+  }
+`;
+
+const FormBox = ({ children }) => {
+  return <Container>{children}</Container>;
+};
+
+export default FormBox;
