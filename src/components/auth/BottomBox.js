7c5d2154f99c9c017b11128b7929b823141bb2c1
sjwhole <bluemoon2718@naver.com>
2021-08-25 19:58:51 +0900

    feat: Add bottom box component

---
+++ src/components/auth/BottomBox.js
@@ -0,0 +1,32 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import { BaseBox } from "../shared";
+import PropTypes from "prop-types";
+
+const SBottomBox = styled(BaseBox)`
+  padding: 20px 40px;
+  text-align: center;
+  width: 100%;
+  a {
+    font-weight: 600;
+    color: ${(props) => props.theme.blue};
+    margin-left: 2px;
+  }
+`;
+
+const BottomBox = ({ cta, link, linkText }) => {
+  return (
+    <SBottomBox>
+      <span>{cta}</span>
+      <Link to={link}>{linkText}</Link>
+    </SBottomBox>
+  );
+};
+
+BottomBox.prototype = {
+  cta: PropTypes.string.isRequired,
+  link: PropTypes.string.isRequired,
+  linkTo: PropTypes.string.isRequired,
+};
+
+export default BottomBox;
