3b4a2b5f57244be6723fa7fb55c8d6fae7cb6942
sjwhole <bluemoon2718@naver.com>
2021-08-25 20:00:25 +0900

    feat: Add routes to app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
+import { useReactiveVar } from "@apollo/client";
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import { darkModeVar, isLoggedInVar } from "./apollo";
+import routes from "./routes";
+import Home from "./screens/Home";
+import Login from "./screens/Login";
+import NotFound from "./screens/NotFound";
+import SignUp from "./screens/SignUp";
+import { lightTheme, darkTheme, GlobalStyles } from "./styles";
 
 const App = () => {
-  return <div>Hello Word</div>;
+  const isLoggedIn = useReactiveVar(isLoggedInVar);
+  const darkMode = useReactiveVar(darkModeVar);
+  return (
+    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+      <GlobalStyles />
+      <Router>
+        <Switch>
+          <Route exact path={routes.home}>
+            {isLoggedIn ? <Home /> : <Login />}
+          </Route>
+          {!isLoggedIn ? (
+            <Route path={routes.signUp}>
+              <SignUp />
+            </Route>
+          ) : null}
+          <Route>
+            <NotFound />
+          </Route>
+        </Switch>
+      </Router>
+    </ThemeProvider>
+  );
 };
 
 export default App;
