53cc047b97127509fbfac29bac4104fda8338fd9
sjwhole <bluemoon2718@naver.com>
2021-08-27 23:04:33 +0900

    feat: Add client & login logout

--- src/apollo.js
+++ src/apollo.js
@@ -1,4 +1,23 @@
-import { makeVar } from "@apollo/client";
+import { ApolloClient, InMemoryCache, makeVar } from "@apollo/client";
 
-export const isLoggedInVar = makeVar(false);
+const TOKEN = "token";
+
+export const isLoggedInVar = makeVar(Boolean(localStorage.getItem(TOKEN)));
+
+export const logUserIn = (token) => {
+  localStorage.setItem(TOKEN, token);
+  isLoggedInVar(true);
+};
+
+export const logUserOut = () => {
+  localStorage.removeItem(TOKEN);
+  isLoggedInVar(false);
+  window.location.reload();
+};
+
 export const darkModeVar = makeVar(false);
+
+export const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql",
+  cache: new InMemoryCache(),
+});
