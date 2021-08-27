0b8de0b871007ff01972606102e9dad804d3cd6d
sjwhole <bluemoon2718@naver.com>
2021-08-27 23:07:00 +0900

    feat: Add home page logout

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,3 +1,10 @@
-const Home = () => <h1>Home</h1>;
+import { logUserOut } from "../apollo";
+
+const Home = () => (
+  <div>
+    <h1>Home</h1>
+    <button onClick={() => logUserOut()}>Log Out</button>
+  </div>
+);
 
 export default Home;
