4513e1cf7b276493e0462ed6827078b423ae3cf6
sjwhole <bluemoon2718@naver.com>
2021-08-27 23:06:52 +0900

    feat: Add sign up apollo

--- src/screens/SignUp.js
+++ src/screens/SignUp.js
@@ -8,6 +8,10 @@ import Input from "../components/auth/Input";
 import FormBox from "../components/auth/FormBox";
 import BottomBox from "../components/auth/BottomBox";
 import { FatLink } from "../components/shared";
+import PageTitle from "../components/PageTitle";
+import { useForm } from "react-hook-form";
+import { gql, useMutation } from "@apollo/client";
+import { useHistory } from "react-router";
 
 const HeaderContainer = styled.div`
   display: flex;
@@ -20,9 +24,63 @@ const Subtitle = styled(FatLink)`
   text-align: center;
 `;
 
+const CREATE_ACCOUNT_MUTATION = gql`
+  mutation createAccount(
+    $firstName: String!
+    $lastName: String
+    $username: String!
+    $email: String!
+    $password: String!
+  ) {
+    createAccount(
+      firstName: $firstName
+      lastName: $lastName
+      username: $username
+      email: $email
+      password: $password
+    ) {
+      ok
+      error
+    }
+  }
+`;
+
 const SignUp = () => {
+  const history = useHistory();
+  const onCompleted = (data) => {
+    const { username, password } = getValues();
+    const {
+      createAccount: { ok, error },
+    } = data;
+    if (!ok) {
+      return;
+    }
+    history.push(routes.home, {
+      message: "Account is created. Please log in.",
+      username,
+      password,
+    });
+  };
+  const [createAccount, { loading }] = useMutation(CREATE_ACCOUNT_MUTATION, {
+    onCompleted,
+  });
+  const { register, handleSubmit, errors, formState, getValues } = useForm({
+    mode: "onChange",
+  });
+  const onSubmitVaild = (data) => {
+    if (loading) {
+      return;
+    }
+    createAccount({
+      variables: {
+        ...data,
+      },
+    });
+  };
+
   return (
     <AuthLayout>
+      <PageTitle title="Sign up" />
       <FormBox>
         <HeaderContainer>
           <FontAwesomeIcon icon={faInstagram} size="3x" />
@@ -30,12 +88,42 @@ const SignUp = () => {
             Sign up to see photos and videos from your friends.
           </Subtitle>
         </HeaderContainer>
-        <form>
-          <Input type="text" placeholder="Email" />
-          <Input type="text" placeholder="Name" />
-          <Input type="text" placeholder="username" />
-          <Input type="password" placeholder="password" />
-          <Button type="submit" value="Sign up" />
+        <form onSubmit={handleSubmit(onSubmitVaild)}>
+          <Input
+            ref={register({ required: "First Name is required." })}
+            name="firstName"
+            type="text"
+            placeholder="First Name"
+          />
+          <Input
+            ref={register}
+            name="lastName"
+            type="text"
+            placeholder="Last Name"
+          />
+          <Input
+            ref={register({ required: "Email is required." })}
+            name="email"
+            type="text"
+            placeholder="Email"
+          />
+          <Input
+            ref={register({ required: "Username is required." })}
+            name="username"
+            type="text"
+            placeholder="username"
+          />
+          <Input
+            ref={register({ required: "Password is required." })}
+            name="password"
+            type="password"
+            placeholder="password"
+          />
+          <Button
+            type="submit"
+            value={loading ? "Loading..." : "Sign up"}
+            disabled={!formState.isValid || loading}
+          />
         </form>
       </FormBox>
       <BottomBox cta="Have an account" linkText="Log in" link={routes.home} />
