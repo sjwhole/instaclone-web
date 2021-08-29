b0f98b2c78a53ea4e3ba0c9347d268105a94f194
sjwhole <bluemoon2718@naver.com>
2021-08-30 00:25:50 +0900

    feat: Add footer to auth layout

--- src/components/auth/AuthLayout.js
+++ src/components/auth/AuthLayout.js
@@ -1,4 +1,8 @@
 import styled from "styled-components";
+import { useReactiveVar } from "@apollo/client";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMoon, faSun } from "@fortawesome/free-regular-svg-icons";
+import { darkModeVar, disableDarkMode, enableDarkMode } from "../../apollo";
 
 const Container = styled.div`
   display: flex;
@@ -13,10 +17,24 @@ const Wrapper = styled.div`
   width: 100%;
 `;
 
+const Footer = styled.footer`
+  margin-top: 20px;
+`;
+
+const DarkModeBtn = styled.span`
+  cursor: pointer;
+`;
+
 const AuthLayout = ({ children }) => {
+  const darkMode = useReactiveVar(darkModeVar);
   return (
     <Container>
       <Wrapper>{children}</Wrapper>
+      <Footer>
+        <DarkModeBtn onClick={darkMode ? disableDarkMode : enableDarkMode}>
+          <FontAwesomeIcon icon={darkMode ? faSun : faMoon} />
+        </DarkModeBtn>
+      </Footer>
     </Container>
   );
 };
