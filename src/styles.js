db45ad7103942a0d2a2f69d740af0d6f842022dc
sjwhole <bluemoon2718@naver.com>
2021-08-30 00:25:02 +0900

    feat: Add styles

--- src/styles.js
+++ src/styles.js
@@ -3,11 +3,13 @@ import reset from "styled-reset";
 
 export const lightTheme = {
   blue: "#0095f6",
+  bgColor: "#FAFAFA",
+  fontColor: "rgb(38, 38, 38)",
   borderColor: "rgb(219, 219, 219)",
 };
 export const darkTheme = {
   fontColor: "white",
-  bgColor: "lightgray",
+  bgColor: "#2c2c2c",
 };
 
 export const GlobalStyles = createGlobalStyle`
@@ -19,9 +21,10 @@ export const GlobalStyles = createGlobalStyle`
       box-sizing:border-box;
     }
   body {
-    background-color: #FAFAFA;
-        font-size:14px;
-        font-family:'Open Sans', sans-serif;
+      background-color: ${(props) => props.theme.bgColor};
+      font-size:14px;
+      font-family:'Open Sans', sans-serif;
+      color: ${(props) => props.theme.fontColor}
     }
     a {
       text-decoration: none;
