073568359fd8859b7a8570916d6425f4b6fc9ce1
sjwhole <bluemoon2718@naver.com>
2021-08-31 22:04:46 +0900

    feat: Add photo

---
+++ src/components/feed/Photo.js
@@ -0,0 +1,182 @@
+import React from "react";
+import styled from "styled-components";
+import { FatText } from "../shared";
+import Avatar from "../Avatar";
+import PropTypes from "prop-types";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBookmark,
+  faComment,
+  faHeart,
+  faPaperPlane,
+} from "@fortawesome/free-regular-svg-icons";
+import { faHeart as SolidHeart } from "@fortawesome/free-solid-svg-icons";
+import { gql, useMutation } from "@apollo/client";
+import Comments from "./Comments";
+import { Link } from "react-router-dom";
+
+const TOGGLE_LIKE_MUTATION = gql`
+  mutation toggleLike($id: Int!) {
+    toggleLike(id: $id) {
+      ok
+      error
+    }
+  }
+`;
+
+const PhotoContainer = styled.div`
+  background-color: white;
+  border: 1px solid ${(props) => props.theme.borderColor};
+  margin-bottom: 20px;
+  width: 80%;
+`;
+
+const PhotoHeader = styled.div`
+  padding: 15px 20px;
+  display: flex;
+  align-items: center;
+`;
+
+const Username = styled(FatText)`
+  margin-left: 5px;
+`;
+
+const PhotoFile = styled.img`
+  min-width: 100%;
+  max-width: 100%;
+`;
+
+const PhotoData = styled.div``;
+
+const PhotoActions = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 15px;
+  div {
+    display: flex;
+    align-items: center;
+  }
+`;
+
+const PhotoAction = styled.div`
+  margin-right: 10px;
+  cursor: pointer;
+`;
+
+const Likes = styled(FatText)`
+  margin-top: 15px;
+  display: block;
+`;
+
+const Photo = ({
+  id,
+  user,
+  file,
+  isLiked,
+  likes,
+  caption,
+  commentNumber,
+  comments,
+}) => {
+  const updateToggleLike = (cache, result) => {
+    const {
+      data: {
+        toggleLike: { ok },
+      },
+    } = result;
+
+    if (ok) {
+      const photoId = `Photo:${id}`;
+      cache.modify({
+        id: photoId,
+        fields: {
+          isLiked: (prev) => !prev,
+          likes: (prev) => {
+            if (isLiked) {
+              return prev - 1;
+            } else {
+              return prev + 1;
+            }
+          },
+        },
+      });
+    }
+  };
+  const [toggleLikeMutation] = useMutation(TOGGLE_LIKE_MUTATION, {
+    variables: {
+      id,
+    },
+    update: updateToggleLike,
+  });
+  return (
+    <PhotoContainer key={id}>
+      <PhotoHeader>
+        <Link to={`/users/${user.username}`}>
+          <Avatar lg url={user.avatar} />
+        </Link>
+        <Link to={`/users/${user.username}`}>
+          <Username>{user.username}</Username>
+        </Link>
+      </PhotoHeader>
+      <PhotoFile src={file} />
+      <PhotoData>
+        <PhotoActions>
+          <div>
+            <PhotoAction onClick={toggleLikeMutation}>
+              <FontAwesomeIcon
+                size={"2x"}
+                style={{ color: isLiked ? "tomato" : "inherit" }}
+                icon={isLiked ? SolidHeart : faHeart}
+              />
+            </PhotoAction>
+            <PhotoAction>
+              <FontAwesomeIcon size={"2x"} icon={faComment} />
+            </PhotoAction>
+            <PhotoAction>
+              <FontAwesomeIcon size={"2x"} icon={faPaperPlane} />
+            </PhotoAction>
+          </div>
+          <div>
+            <FontAwesomeIcon size={"2x"} icon={faBookmark} />
+          </div>
+        </PhotoActions>
+        <Likes>{likes === 1 ? "1 like" : `${likes} likes`}</Likes>
+        <Comments
+          photoId={id}
+          author={user.username}
+          caption={caption}
+          comments={comments}
+          commentNumber={commentNumber}
+        />
+      </PhotoData>
+    </PhotoContainer>
+  );
+};
+
+Photo.propTypes = {
+  id: PropTypes.number.isRequired,
+  user: PropTypes.shape({
+    avatar: PropTypes.string,
+    username: PropTypes.string.isRequired,
+  }).isRequired,
+  file: PropTypes.string.isRequired,
+  isLiked: PropTypes.bool.isRequired,
+  likes: PropTypes.number.isRequired,
+  caption: PropTypes.string.isRequired,
+  commentNumber: PropTypes.number.isRequired,
+  comments: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      user: PropTypes.shape({
+        avatar: PropTypes.string.isRequired,
+        username: PropTypes.string.isRequired,
+      }),
+      payload: PropTypes.string.isRequired,
+      isMine: PropTypes.bool.isRequired,
+      createdAt: PropTypes.string.isRequired,
+    })
+  ),
+};
+
+export default Photo;
