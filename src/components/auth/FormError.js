3831707b491e9a9bcdb6d88d65ba8eab5a5d99fc
sjwhole <bluemoon2718@naver.com>
2021-08-31 22:03:48 +0900

    feat: Add FormError proptypes

--- src/components/auth/FormError.js
+++ src/components/auth/FormError.js
@@ -1,3 +1,5 @@
+import React from "react";
+import { PropTypes } from "prop-types";
 import styled from "styled-components";
 
 const SFormError = styled.span`
@@ -9,6 +11,10 @@ const SFormError = styled.span`
 
 const FormError = ({ message }) => {
   return message === "" || !message ? null : <SFormError>{message}</SFormError>;
+};
+
+FormError.propTypes = {
+  message: PropTypes.any,
 };
 
 export default FormError;
