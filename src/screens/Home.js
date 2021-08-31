331f41385858bb4948eb0f50bd983a60fb6bedc4
sjwhole <bluemoon2718@naver.com>
2021-08-31 22:05:11 +0900

    refator: Clean home screen

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,35 +1,29 @@
+import React from "react";
 import { gql, useQuery } from "@apollo/client";
 import styled from "styled-components";
-import Avatar from "../components/Avatar";
-import { FatText } from "../components/shared";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faBookmark,
-  faComment,
-  faHeart,
-  faPaperPlane,
-} from "@fortawesome/free-regular-svg-icons";
-import { faHeart as SolidHeart } from "@fortawesome/free-solid-svg-icons";
 import PageTitle from "../components/PageTitle";
+import Photo from "../components/feed/Photo";
+import { COMMENT_FRAGMENT, PHOTO_FRAGMENT } from "../fragments";
 
-const FEED_QUERY = gql`
+export const FEED_QUERY = gql`
   query seeFeed {
     seeFeed {
-      id
+      ...PhotoFragment
       user {
         username
         avatar
       }
-      file
       caption
-      likes
-      comments
+      comments {
+        ...CommentFragment
+      }
       createdAt
       updatedAt
       isMine
-      isLiked
     }
   }
+  ${PHOTO_FRAGMENT}
+  ${COMMENT_FRAGMENT}
 `;
 
 const HomeContainer = styled.div`
@@ -38,87 +32,14 @@ const HomeContainer = styled.div`
   align-items: center;
 `;
 
-const PhotoContainer = styled.div`
-  background-color: white;
-  border: 1px solid ${(props) => props.theme.borderColor};
-  margin-bottom: 20px;
-  width: 100%;
-`;
-
-const PhotoHeader = styled.div`
-  padding: 15px 20px;
-  display: flex;
-  align-items: center;
-`;
-
-const Username = styled(FatText)`
-  margin-left: 5px;
-`;
-
-const PhotoFile = styled.img`
-  min-width: 100%;
-  max-width: 100%;
-`;
-
-const PhotoData = styled.div``;
-
-const PhotoActions = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding: 15px;
-  div {
-    display: flex;
-    align-items: center;
-  }
-`;
-
-const PhotoAction = styled.div`
-  margin-right: 10px;
-`;
-
-const Likes = styled(FatText)``;
-
 const Home = () => {
   const { data } = useQuery(FEED_QUERY);
-  console.log(data);
   return (
     <>
       <PageTitle title="Home" />
       <HomeContainer>
         {data?.seeFeed?.map((photo) => (
-          <PhotoContainer key={photo.id}>
-            <PhotoHeader>
-              <Avatar lg url={photo.user.avatar} />
-              <Username>{photo.user.username}</Username>
-            </PhotoHeader>
-            <PhotoFile src={photo.file} />
-            <PhotoData>
-              <PhotoActions>
-                <div>
-                  <PhotoAction>
-                    <FontAwesomeIcon
-                      size={"2x"}
-                      style={{ color: photo.isLiked ? "tomato" : "inherit" }}
-                      icon={photo.isLiked ? SolidHeart : faHeart}
-                    />
-                  </PhotoAction>
-                  <PhotoAction>
-                    <FontAwesomeIcon size={"2x"} icon={faComment} />
-                  </PhotoAction>
-                  <PhotoAction>
-                    <FontAwesomeIcon size={"2x"} icon={faPaperPlane} />
-                  </PhotoAction>
-                </div>
-                <div>
-                  <FontAwesomeIcon size={"2x"} icon={faBookmark} />
-                </div>
-              </PhotoActions>
-              <Likes>
-                {photo.likes === 1 ? "1 like" : `${photo.likes} likes`}
-              </Likes>
-            </PhotoData>
-          </PhotoContainer>
+          <Photo key={photo.id} {...photo} />
         ))}
       </HomeContainer>
     </>
