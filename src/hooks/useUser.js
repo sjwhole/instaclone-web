b27b9596b85bcedd1cda129eb6979fea1d9d821d
sjwhole <bluemoon2718@naver.com>
2021-08-31 22:04:59 +0900

    feat: Add ME_QUERY export

--- src/hooks/useUser.js
+++ src/hooks/useUser.js
@@ -2,7 +2,7 @@ import { gql, useQuery, useReactiveVar } from "@apollo/client";
 import { useEffect } from "react";
 import { isLoggedInVar, logUserOut } from "../apollo";
 
-const ME_QUERY = gql`
+export const ME_QUERY = gql`
   query me {
     me {
       username
